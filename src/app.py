f52d11b1e5b500a682f79f8d75b8dc0a4f5009f9
08786218654 <leandrosena@lampp-it.com.br>
2024-05-05 16:54:21 -0300

    push

--- src/app.py
+++ src/app.py
@@ -11,6 +11,7 @@ app.add_url_rule(routes["index_route"], view_func=routes["index_controller"])
 app.add_url_rule(routes["delete_route"], view_func=routes["delete_controller"])
 app.add_url_rule(routes["update_route"], view_func=routes["update_controller"])
 app.add_url_rule(routes["get_paciente_route"], view_func=routes["get_paciente_controller"])
+app.add_url_rule(routes["create_paciente_route"], view_func=routes["create_paciente_controller"])
 
 @app.route("/<path:unknown>")
 def not_found(unknown):
--- src/static/js/script.js
+++ src/static/js/script.js
@@ -3,3 +3,9 @@ $(document).ready(function($) {
     $('#cpfInput').mask('000.000.000-00');
 });
 
+
+$(document).ready(function($) {
+    $('#dataNascimento').mask('0000-00-00');
+});
+
+
