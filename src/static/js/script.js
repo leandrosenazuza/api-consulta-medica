e65db89c4b968a0763b38fdac9228931038b016f
08786218654 <leandrosena@lampp-it.com.br>
2024-05-13 16:05:48 -0300

    Atualização funcionando

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -10,3 +10,7 @@ $(document).ready(function($) {
     $('.cpf-mask').mask('000.000.000-00');
     $('.date-mask').mask('0000-00-00');
 });
+
+$(document).ready(function() {
+    $('.cpf-mask').mask('000.000.000-00');
+});
